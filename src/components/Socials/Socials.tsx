456f3f8181d07a1a63bbc06b37ea0fa823e0fce4
Yuri Carvalho <carvalhoyc.dev@outlook.com.br>
2025-06-07 13:48:32 -0300

    Ajustes em produção

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -6,12 +6,14 @@ function Socials() {
       <ul>
         <a
           href="https://www.linkedin.com/in/yuri-carvalho-de-oliveira/"
+          target="_blank"
           aria-label="Botão que redireciona para o meu Linkedin"
         >
          <img className="icon-social" src="./images/linkedin-plain.svg" alt="icone do linkedin"/>
         </a>
         <a
           href="https://github.com/YuOliveira-dev"
+          target="_blank"
           aria-label="Botão que redireciona para o meu GitHub"
         >
         
@@ -20,6 +22,7 @@ function Socials() {
         </a>
         <a
           href="https://medium.com/@carvalhoyc"
+          target="_blank"
           aria-label="Botão que redireciona para o meu Medium"
         >
           <img className="icon-social" src="./images/Medium Icon 1.svg" alt="icone do medium" />
